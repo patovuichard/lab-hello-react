7cf61c80e1a36b9c6e0d6d5e9f798791bdc90ec7
Patricio Sanchez Vuichard <patriciosanchezvuichard@gmail.com>
2023-02-27 17:55:45 +0100

    la done

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -11,11 +11,11 @@ function App() {
 
   const awesomeStyle = {
     backgroundColor: "white",
-    color: "gray",
-    width: "150px",
+    color: "rgb(44, 44, 47)",
+    width: "100px",
     padding: "10px",
-    borderRadius: "5px"
-
+    borderRadius: "5px",
+    textAlign: "center",
   }
 
   return (
@@ -28,35 +28,33 @@ function App() {
       
       <section id='top'>
 
-        <h1>Say hello to ReactJS</h1>
-        <p>You will learn how to use</p>
-        <p>the most popular frontend library,</p>
-        <p>and became a super Ninja developer</p>
-        <div style={awesomeStyle}>Awesome</div>
+        <h1 style={{fontSize: "5rem", width: "500px", marginBottom: "10px"}}>Say hello to ReactJS</h1>
+        <p style={{width: "200px", marginBottom: "40px"}}>You will learn how to use the most popular frontend library, and became a super Ninja developer</p>
+        <div style={awesomeStyle}>Awesome!</div>
 
       </section>
 
       <section id='bottom'>
 
-        <div>
+        <div style={{width: "150px"}}>
           <img src={icon1} alt="icon1" width="100px"/>
           <h3>Declarative</h3>
-          <p>React makes it lainless to create interactive UIs</p>
+          <p>React makes it painless to create interactive UIs</p>
         </div>
 
-        <div>
+        <div style={{width: "150px"}}>
           <img src={icon2} alt="icon2" width="100px"/>
           <h3>Components</h3>
           <p>Build encapsulated component that manage their state</p>  
         </div>
 
-        <div>
+        <div style={{width: "150px"}}>
           <img src={icon3} alt="icon3" width="100px"/>
           <h3>Single-Way</h3>
           <p>A set of immutable values are passed to their component´s</p>
         </div>
 
-        <div>
+        <div style={{width: "150px"}}>
           <img src={icon4} alt="icon4" width="100px"/>
           <h3>JSX</h3>
           <p>Statically-typed, desgined to run on modern browsers</p>
